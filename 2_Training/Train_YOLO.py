ddc67b5cc9529d338cc23b5182d3e4baf9aa847e
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-29 14:10:24 +0530

    trim dataset to smaller value for faster training

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--epochs",
         type=int,
-        default=51,
+        default=5,
         help="Number of epochs for training last layers and number of epochs for fine-tuning layers. Default is 51.",
     )
     parser.add_argument(
