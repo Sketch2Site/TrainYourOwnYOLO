1f53bc24c2c03e3e50194cd42f5f1541d4d14740
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-29 16:07:08 +0530

    increase epoch to 50

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--epochs",
         type=int,
-        default=5,
+        default=50,
         help="Number of epochs for training last layers and number of epochs for fine-tuning layers. Default is 51.",
     )
     parser.add_argument(
